a05fdc249f8a1b80a7788d144c2b8020cb003230
srutownik <marek.28.93@gmail.com>
2017-05-06 17:44:24 +0200

    AutonomicAgent extraction and collision detection

--- Boids.py
+++ Boids.py
@@ -8,8 +8,9 @@ from Types import t_matcher
 class BaseAgent:
     vision = 2
 
-    def __init__(self, unique_id, space, x, y):
+    def __init__(self, unique_id, space, x, y, r):
         super().__init__()
+        self.r = r
         self.unique_id = unique_id
         self.space = space
         self.pos = (x, y)
@@ -23,9 +24,40 @@ class BaseAgent:
         pass
 
 
+class AutonomicAgent(BaseAgent):
+    def __init__(self, unique_id, space, x, y, r, max_speed, heading=None):
+        super().__init__(unique_id, space, x, y, r)
+        self.max_speed = max_speed
+        self.heading = heading if heading else np.random.random(2)
+
+        self.new_pos = None
+
+    def step(self):
+        self.update_heading(self.space.get_neighbors(self.pos, BaseAgent.vision, False))
+        new_pos = np.array(self.pos) + self.heading * self.max_speed
+        (new_x, new_y) = new_pos
+        self.new_pos = self.space.torus_adj((new_x, new_y))
+
+    def advance(self):
+        if self.valid_decision():
+            self.space.move_agent(self, self.new_pos)
+
+    def valid_decision(self):
+        return not colliding_decision(self)
+
+    def update_heading(self, neighbours):
+        # noinspection PyCallingNonCallable
+        new_heading = self.distributed_decision()(np.array(self.pos), neighbours)
+        self.heading += norm(new_heading) * 0.3
+        self.heading = norm(self.heading)
+
+    def distributed_decision(self):
+        pass
+
+
 class MarkerAgent(BaseAgent):
     def __init__(self, unique_id, space, x, y):
-        super().__init__(unique_id, space, x, y)
+        super().__init__(unique_id, space, x, y, r=0.02)
         self.r = 0.02
 
     def draw(self):
@@ -40,7 +72,7 @@ class MarkerAgent(BaseAgent):
 
 class GrassAgent(BaseAgent):
     def __init__(self, unique_id, space, x, y):
-        super().__init__(unique_id, space, x, y)
+        super().__init__(unique_id, space, x, y, r=0.06)
         self.r = 0.06
 
     def draw(self):
@@ -55,7 +87,7 @@ class GrassAgent(BaseAgent):
 
 class WolfAgent(BaseAgent):
     def __init__(self, unique_id, space, x, y):
-        super().__init__(unique_id, space, x, y)
+        super().__init__(unique_id, space, x, y, r=0.14)
         self.r = 0.14
 
     def draw(self):
@@ -68,52 +100,26 @@ class WolfAgent(BaseAgent):
         pass
 
 
-class SheepAgent(BaseAgent):
+class SheepAgent(AutonomicAgent):
     def __init__(self, unique_id, space, x, y, max_speed=0.03, heading=None):
-        super().__init__(unique_id, space, x, y)
-        self.max_speed = max_speed
-        self.heading = heading if heading else np.random.random(2)
+        super().__init__(unique_id, space, x, y, r=0.1, max_speed=max_speed, heading=heading)
         self.asd = [i for (i, c) in enumerate([33, 22, 33]) for _ in range(c)]
-        self.r = 0.1
 
-        self.new_pos = None
+        self.decisions = [Flocking(SheepAgent, 1.5), Eating(GrassAgent), escaping]
 
     def draw(self):
         return {'Color': 'blue', 'rs': BaseAgent.vision}
 
-    def step(self):
-        self.update_heading(self.space.get_neighbors(self.pos, BaseAgent.vision, False))
-        new_pos = np.array(self.pos) + self.heading * self.max_speed
-        (new_x, new_y) = new_pos
-        self.new_pos = self.space.torus_adj((new_x, new_y))
-
-    def advance(self):
-        if self.valid_decision():
-            self.space.move_agent(self, self.new_pos)
-
-    def update_heading(self, neighbours):
-        new_heading = self.distributed_decision()(np.array(self.pos), neighbours)
-        self.heading += norm(new_heading) * 0.3
-        self.heading = norm(self.heading)
-
     def distributed_decision(self):
         self.decision = np.random.choice(self.asd)
-        return decisions[self.decision]
-
-    def valid_decision(self):
-        neighbors = self.space.get_neighbors(self.new_pos, self.r * 2)
-        neighbors = list(filter(t_matcher(SheepAgent), neighbors))
-        neighbors.remove(self)
-        return not neighbors
+        return self.decisions[self.decision]
 
 
-def eating(me, neighbours):
-    neighbours = list(filter(t_matcher(GrassAgent), neighbours))
-    if len(neighbours):
-        return min(neighbours_vectors(me, neighbours),
-                   key=sqr_dst)
-    else:
-        return np.array([0, 0])
+def colliding_decision(agent):
+    neighbours = agent.space.get_neighbors(agent.new_pos, agent.r * 2)
+    neighbours = list(filter(t_matcher(type(agent)), neighbours))
+    neighbours.remove(agent)
+    return not not neighbours
 
 
 def escaping(me, neighbours):
@@ -122,4 +128,14 @@ def escaping(me, neighbours):
                np.array([0, 0]))
 
 
-decisions = [Flocking(SheepAgent, 1.5), eating, escaping]
+class Eating:
+    def __init__(self, food_type):
+        self.food_type = food_type
+
+    def __call__(self, me, neighbours):
+        neighbours = list(filter(t_matcher(self.food_type), neighbours))
+        if len(neighbours):
+            return min(neighbours_vectors(me, neighbours),
+                       key=sqr_dst)
+        else:
+            return np.array([0, 0])
