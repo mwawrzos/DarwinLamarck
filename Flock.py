3b27b62484a6dedcaeae599c8ee74a76cc182d1b
srutownik <marek.28.93@gmail.com>
2017-06-21 18:50:09 +0200

    Flock refinement; Separate extraction

--- Flock.py
+++ Flock.py
@@ -11,41 +11,46 @@ def match(neighbours):
 
 
 class Flock:
-    def __init__(self, me, space, coherence_w=1, separate_w=1, match_w=1, separation=1.5):
-        self.me = me
+    def __init__(self, agent, space, coherence_w=1, separate_w=1, match_w=1, separation=1.5):
+        self.agent = agent
         self.space = space
         self.coherence_w = coherence_w
         self.separate_w = separate_w
         self.match_w = match_w
-        self.separation = separation
-
-    def separation_scaling(self, vector):
-        length = v_len(vector)
-        return vector * (self.separation - length) / length
-
-    def separation_vector(self, neighbour_vector):
-        length = v_len(neighbour_vector)
-        return neighbour_vector * (self.separation - length) / length
-
-    def separate(self, neighbours):
-        def close_enough(neighbour):
-            return self.space.get_distance(self.me.pos, neighbour) < self.separation
-
-        close_neighbours = filter(close_enough, map(pos_vector, neighbours))
-        neighbours_vectors = map(vector2d(self.space),
-                                 close_neighbours,
-                                 itertools.repeat(self.me.pos))
-        neighbours_separation = map(self.separation_vector, neighbours_vectors)
-        return sum(neighbours_separation)
+        self.separate = Separate(agent, space, separation)
 
     def __call__(self, neighbours):
-        neighbours = list(filter(t_matcher(type(self.me)), neighbours))
-        coherence = self.cohere(neighbours) * self.coherence_w
+        neighbours = list(filter(t_matcher(type(self.agent)), neighbours))
+        coherence = cohere(self.agent, self.space, neighbours) * self.coherence_w
         separation = self.separate(neighbours) * self.separate_w
         matching = match(neighbours) * self.match_w
         return coherence + separation + matching
 
-    def cohere(self, neighbours):
-        i = map(vector2d(self.space), itertools.repeat(self.me.pos), map(pos_vector, neighbours))
-        neighbours_vector = sum(i, np.array([0, 0]))
-        return norm(neighbours_vector)
+
+def cohere(agent, space, neighbours):
+    i = map(vector2d(space), itertools.repeat(agent.pos), map(pos_vector, neighbours))
+    neighbours_vector = sum(i, np.array([0, 0]))
+    return norm(neighbours_vector)
+
+
+class Separate:
+    def __init__(self, agent, space, separation=1.5):
+        self.agent = agent
+        self.separation = separation
+        self.space = space
+
+    def __call__(self, neighbours):
+        close_neighbours = filter(self.close_enough, map(pos_vector, neighbours))
+        neighbours_vectors = map(vector2d(self.space),
+                                 close_neighbours,
+                                 itertools.repeat(self.agent.pos))
+        neighbours_separation = map(self.separation_vector, neighbours_vectors)
+        # noinspection PyTypeChecker
+        return sum(neighbours_separation, np.array([0, 0]))
+
+    def close_enough(self, neighbour):
+        return self.space.get_distance(self.agent.pos, neighbour) < self.separation
+
+    def separation_vector(self, neighbour_vector):
+        length = v_len(neighbour_vector)
+        return neighbour_vector * (self.separation - length) / length
