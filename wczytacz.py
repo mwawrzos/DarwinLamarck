4e888e0bed48a9a6153319754834a1605071d8e6
srutownik <marek.28.93@gmail.com>
2017-07-22 21:21:16 +0200

    mesa update in recording loader

--- wczytacz.py
+++ wczytacz.py
@@ -1,14 +1,14 @@
 from gen import GenState
 from visualization.VizualizationServer import *
 
-s = GenState(r'cp\darwin_owce_wilki_1010\019.pkl')
+s = GenState(r'cp\lamarck\019.pkl')
 wolf_agent = WolfAgent, None, s.wolfs
 sheep_agent = SheepAgent, None, s.sheep
 server = ModularServer(SimulationModel,
                        [agent_canvas, histogram, histogram2, VerySimpleText()],
                        "Simulation",
-                       x_max=10,
-                       y_max=10,
-                       species=[grass_agent, sheep_agent, wolf_agent],
-                       iterations=2000)
+                       {'x_max': 10,
+                        'y_max': 10,
+                        'species': [grass_agent, sheep_agent, wolf_agent],
+                        'iterations': 2000})
 server.launch()
